258285c2f809494d7091ffa24bf00da86744d4e3
Ayaz-Mohammed-Khan <ayazmohdkhan21@gmail.com>
2023-01-09 23:15:21 +0530

    added btns functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Login from "./components/Login"
-import "./App.css"
-import logo from "./images/logo.png"
+import React from "react";
+import Login from "./components/Login";
+import "./App.css";
+import logo from "./images/logo.png";
+import LoginP from "./components/LoginP";
 
-export default function App(){
-    return(
-      <div>
-        <Login logo={logo} />
-
-      </div>
-    )
+export default function App() {
+  const [isTrue, setIsTrue] = React.useState(false);
+  function handler(id) {
+    console.log(id);
+    setIsTrue((prev) => !prev);
+  }
+  return (
+    <div>
+      {!isTrue && <LoginP func={handler} logo={logo} />}
+      {isTrue && <Login logo={logo} />}
+    </div>
+  );
 }
-
-
