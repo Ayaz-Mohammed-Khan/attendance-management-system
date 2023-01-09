0d9d7d8eec9a8bf3d420738e8f50475e8187b6e3
Ayaz-Mohammed-Khan <ayazmohdkhan21@gmail.com>
2023-01-10 00:01:50 +0530

    reactivity added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,29 @@
-export default function Login(props){
-    return(
-        <div>
-            <div id="main">
-                <div id="text">
-                <img id="logo" alt="attendence-management" src={props.logo}/>
-                <h1>Attendence Management System</h1>
-            </div>
-            <div  id="form">
-                <form id="login" >
-                    <div>
-                        <h3>Login</h3>
-                        <button id="admin">Admin</button>
-                        <button id="faculty">Faculty</button><br/><br/>
-                        <label for="User-Id">User-Id</label><br/>
-                        <input className="input-data" id="User-Id" type="email" /> <br/>
-                        <label for="Password">Password</label><br/>
-                        <input className="input-data" id="Password" type="password" /><br/>
-                        <input id="submit" value="Login" type="Submit" />  
-                    </div>
-                </form>
-            </div>
-        
-            </div>
+export default function Login(props) {
+  return (
+    <div>
+      <div id="main">
+        <div id="text">
+          <img id="logo" alt="attendence-management" src={props.logo} />
+          <h1>Attendence Management System</h1>
         </div>
-    )
+        <div id="form">
+          <form id="login">
+            <div>
+              <h3>Login</h3>
+              <button>Back</button>
+              <br />
+              <label for="User-Id">User-Id</label>
+              <br />
+              <input className="input-data" id="User-Id" type="email" /> <br />
+              <label for="Password">Password</label>
+              <br />
+              <input className="input-data" id="Password" type="password" />
+              <br />
+              <input id="submit" value="Login" type="Submit" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/LoginP.js
+++ src/components/LoginP.js
@@ -1,5 +1,32 @@
-export default function LoginP(){
-    return(
-        
-    )
+export default function LoginP(props) {
+  console.log(props);
+  return (
+    <div id="main">
+      <div id="text">
+        <img id="logo" alt="attendence-management" src={props.logo} />
+        <h1>Attendence Management System</h1>
+      </div>
+      <div id="form">
+        <div className="login-primery" id="login">
+          <h3>Login</h3>
+          <button
+            onClick={() => props.func("admin")}
+            className="btns"
+            id="admin"
+          >
+            Admin
+          </button>
+          <button
+            onClick={() => props.func("faculty")}
+            className="btns"
+            id="faculty"
+          >
+            Faculty
+          </button>
+          <br />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
 }
